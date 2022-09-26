1ccf9525a02c122b8b40e7ef00be6778bb638892
Paul Osinowo <paulosinowo@Pauls-MacBook-Pro.local>
2022-09-26 01:54:05 +0100

    fix: run scheduler on startup

--- api/apps.py
+++ api/apps.py
@@ -5,7 +5,7 @@ class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
 
-    # def ready(self):
-    #     from . import scheduler
-    #     if settings.SCHEDULER_AUTOSTART:
-    #     	scheduler.start()
+    def ready(self):
+        from . import scheduler
+        if settings.SCHEDULER_AUTOSTART:
+        	scheduler.start()
