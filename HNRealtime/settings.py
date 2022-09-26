bee665fce78be527d786c9e9613c17fd81cf2091
Paul Osinowo <paulosinowo@Pauls-MacBook-Pro.local>
2022-09-26 01:08:37 +0100

    fix: database config

--- HNRealtime/settings.py
+++ HNRealtime/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
     }
 }
 
-DATABASE_URL = os.environ.get('DATABASE_URL')
+DATABASE_URL = config('DATABASE_URL')
 db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
 DATABASES['default'].update(db_from_env)
 
